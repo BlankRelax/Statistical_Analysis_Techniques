172a399f27c9d7c2f11b558a603379949ff6d779
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-10-25 01:16:32 +0100

    finito

--- prob_sheet_4_simpleMC.py
+++ prob_sheet_4_simpleMC.py
@@ -25,73 +25,11 @@ for N in N_array:
 
     plt.hist(y, bins=nBins)
     plt.show()
-    print(np.mean(y))
+    print("Mean is "+ str(np.mean(y)))
     print(np.std(y))
     print(np.mean(rData))
+    print(np.std(rData))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#plt.hist(rData)
-#plt.show()
-#rData_array.append(np.sum(rData))
-#y = np.sqrt(12 / N) * ((np.array(rData_array)) - (N / 2))
-#print(y)
-#plt.hist(y)
-#plt.show()
-
-
-
-
-
-
-
-
-"""
-  # Using transformation method, generate Cauchy distributed numbers
-xMin=-10.
-xMax=10.
-xData = np.tan(np.pi*(rData - 0.5))
-xHist, xbin_edges = np.histogram(xData, bins=nBins, range=(xMin, xMax))
-
-# make plots and save in file
-binLo, binHi = rbin_edges[:-1], rbin_edges[1:]
-xPlot = np.array([binLo, binHi]).T.flatten()
-yPlot = np.array([rHist, rHist]).T.flatten()
-fig, ax = plt.subplots(1,1)
-plt.gcf().subplots_adjust(bottom=0.15)
-plt.gcf().subplots_adjust(left=0.15)
-ax.set_xlim((rMin, rMax))
-ax.set_ylim((0., 150))
-plt.xlabel(r'$r$', labelpad=0)
-plt.plot(xPlot, yPlot)
-
-binLo, binHi = xbin_edges[:-1], xbin_edges[1:]
-xPlot = np.array([binLo, binHi]).T.flatten()
-yPlot = np.array([xHist, xHist]).T.flatten()
-fig, ax = plt.subplots(1,1)
-plt.gcf().subplots_adjust(bottom=0.15)
-plt.gcf().subplots_adjust(left=0.15)
-ax.set_xlim((xMin, xMax))
-ax.set_ylim((0., 800))
-plt.xlabel(r'$x$', labelpad=0)
-plt.plot(xPlot, yPlot)
-
-plt.show()
-#plt.savefig("histograms.pdf", format='pdf')
-"""
 
 
